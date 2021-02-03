286aa65bdf8b60b778b6c02f97e951445fc4a983
BehiceOzturk <behiceyozturk@gmail.com>
2021-02-04 01:52:34 +0300

    First commit

---
+++ DNA Kriptografi/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace DNA_Kriptografi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sifrele = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.sifreCoz = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(125, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 122);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Image = global::DNA_Kriptografi.Properties.Resources._5cc94365ae298ba4616963fc;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Location = new System.Drawing.Point(256, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DNA Kriptografi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(192, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Düz Metin";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(437, 139);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(214, 122);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(484, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Şifrelenmiş Metin";
+            // 
+            // sifrele
+            // 
+            this.sifrele.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.sifrele.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifrele.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.sifrele.Location = new System.Drawing.Point(333, 307);
+            this.sifrele.Name = "sifrele";
+            this.sifrele.Size = new System.Drawing.Size(101, 32);
+            this.sifrele.TabIndex = 3;
+            this.sifrele.Text = "Şifrele";
+            this.sifrele.UseVisualStyleBackColor = false;
+            this.sifrele.Click += new System.EventHandler(this.sifrele_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(125, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kaydırma Sayısı:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(251, 275);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // sifreCoz
+            // 
+            this.sifreCoz.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.sifreCoz.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifreCoz.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.sifreCoz.Location = new System.Drawing.Point(333, 572);
+            this.sifreCoz.Name = "sifreCoz";
+            this.sifreCoz.Size = new System.Drawing.Size(101, 32);
+            this.sifreCoz.TabIndex = 3;
+            this.sifreCoz.Text = "Şifre Çöz";
+            this.sifreCoz.UseVisualStyleBackColor = false;
+            this.sifreCoz.Click += new System.EventHandler(this.sifreCoz_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(168, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Şifrelenmiş Metin";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox4.Location = new System.Drawing.Point(125, 409);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(214, 122);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label6.Location = new System.Drawing.Point(505, 376);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Düz Metin";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox5.Location = new System.Drawing.Point(437, 409);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(214, 122);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label7.Location = new System.Drawing.Point(125, 541);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 21);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Kaydırma Sayısı:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox6.Location = new System.Drawing.Point(251, 542);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::DNA_Kriptografi.Properties.Resources._5cc94365ae298ba4616963fc;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(766, 631);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sifreCoz);
+            this.Controls.Add(this.sifrele);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Name = "Form1";
+            this.Text = "DNA KRİPTOGRAFİ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button sifrele;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button sifreCoz;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ DNA Kriptografi/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNA_Kriptografi
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Sezar()
+        {
+            textBox2.Clear();
+            char[] liste = { 'a','b','c','ç', 'd','e','f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'y', 'z'};
+
+            string metin = textBox1.Text.Trim();
+            int kaydirma = Convert.ToInt16(textBox3.Text);
+            char temp;
+
+
+            foreach (char c in metin.ToLower())
+            {
+
+                temp = c;
+                for (int i = 0; i < 29; i++)
+                {
+                    if (c == liste[i])
+                    {
+                        temp = liste[(i + kaydirma) % 29];
+                    }
+                }
+                textBox2.Text = textBox2.Text + (Convert.ToString(temp));
+
+            }
+        }
+
+        public void Ascii()
+        {
+
+            int i;
+            string metin = textBox2.Text;
+            textBox2.Clear();
+
+
+            for (i = 0; i < metin.Length; i++)
+            {
+                textBox2.Text += Convert.ToInt32(metin[i])+ " ";
+            }
+        }
+
+        public void BinaryValues()
+        {
+            string converted = string.Empty;
+            string metinAscii = textBox2.Text;
+
+            byte[] byteArray = Encoding.ASCII.GetBytes(metinAscii);
+            textBox2.Clear();
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    converted += (byteArray[i] & 0x80) > 0 ? "1" : "0";
+                    byteArray[i] <<= 1;
+                    textBox2.Text = converted;
+                }
+            }
+
+        }
+
+        public void DNA()
+        {
+            string binary = textBox2.Text;
+
+            var codes = new Dictionary<string, string> {
+             {"00", "A"},
+             {"01", "G"},
+             {"10", "C"},
+             {"11", "T"}
+            };
+
+            StringBuilder builder = new StringBuilder();
+            textBox2.Clear();
+            for (int i = 0; i + 1 < binary.Length; i = i + 2)
+            {
+                var localCode = string.Format("{0}{1}", binary[i], binary[i + 1]);
+                string buffer;
+                var output = codes.TryGetValue(localCode, out buffer) ? buffer : string.Empty;
+                builder.Append(output);
+            }
+
+            textBox2.Text = builder.ToString();
+        }
+
+        public void DnaToBinary()
+        {
+            string dna = textBox4.Text.Trim();
+
+            for (int i = 0; i + 1 < dna.Length; i++)
+            {
+                if (dna.Contains("A"))
+                {
+                    dna = dna.Replace("A", "00");
+                }
+
+                if (dna.Contains("G"))
+                {
+                    dna = dna.Replace("G", "01");
+                }
+
+                if (dna.Contains("C"))
+                {
+                    dna = dna.Replace("C", "10");
+                }
+
+                else
+                {
+                    dna = dna.Replace("T", "11");
+                }
+            }
+            textBox5.Text = dna;
+        }
+
+        public Byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                String t = binary.Substring(i, 8);
+
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
+        }
+
+        public void BinaryToAscii()
+        {
+            string binary = textBox5.Text;
+            textBox5.Clear();
+            var data = GetBytesFromBinaryString(binary);
+            var text = Encoding.ASCII.GetString(data);
+            textBox5.Text = text;
+        }
+
+        public void AsciiToText()
+        {
+            string metin = (textBox5.Text).Trim();
+            string[] sayilar = metin.Split(' ');
+            textBox5.Clear();
+            foreach (string sayi in sayilar)
+            {
+                int s1 = Convert.ToInt32(sayi);
+                textBox5.Text += ((char)s1).ToString();              
+            }            
+
+        }
+
+        public void TextToPlainText()
+        {
+            
+            char[] liste1 = { 'a','b','c','ç', 'd','e','f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'y', 'z'};
+
+            string metin1 = textBox5.Text.Trim();
+            int kaydirma1 = Convert.ToInt16(textBox6.Text);
+            char temp1;
+
+            textBox5.Clear();
+            foreach (char c in metin1.ToLower())
+            {
+
+                temp1 = c;
+                for (int i = 0; i < 29; i++)
+                {
+                    if (c == liste1[i])
+                    {
+                        temp1 = liste1[((i - kaydirma1)+29) % 29];
+                    }
+                }
+                textBox5.Text = textBox5.Text + (Convert.ToString(temp1));
+
+            }
+
+        }
+
+        private void sifrele_Click(object sender, EventArgs e)
+        {
+            Sezar();
+            Ascii();
+            BinaryValues();
+            DNA();
+        }
+
+        private void sifreCoz_Click(object sender, EventArgs e)
+        {
+            DnaToBinary();
+            BinaryToAscii();
+            AsciiToText();
+            TextToPlainText();
+        }
+    }
+}
